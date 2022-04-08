a53c6e79e915270571e628c9a5fca71a9e564e80
mickemohlin <mickemohlin@live.se>
2022-04-08 15:31:08 +0200

    Added fragments

--- App/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ App/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val frgConnect = ConnectFragment()
+        val frgControl = ControlFragment()
+        val frgMapHistory = MapHistoryFragment()
+
+        // Set default fragment (ConnectFragment)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentHolder, frgConnect)
+            commit()
+        }
+
+        // Change to ConnectFragment
+        btnConnectFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentHolder, frgConnect)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        // Change to ControlFragment
+        btnControlFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentHolder, frgControl)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        // Change to MapHistoryFragment
+        btnMapHistoryFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentHolder, frgMapHistory)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
     }
 }
---
+++ App/app/src/main/java/com/example/myapplication/MapHistoryFragment.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MapHistoryFragment : Fragment(R.layout.fragment_map_history) {
+
+}
