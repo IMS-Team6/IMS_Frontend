a2dde5904608ab71c8d3011d7c9f7a089270d40d
mickemohlin <mickemohlin@live.se>
2022-05-04 11:16:50 +0200

    App can now fetch sessions and print result to console

--- App/app/src/main/java/com/example/myapplication/MapHistoryFragment.kt
+++ App/app/src/main/java/com/example/myapplication/MapHistoryFragment.kt
@@ -13,7 +13,15 @@ import android.widget.ArrayAdapter
 import android.widget.FrameLayout
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import com.beust.klaxon.Klaxon
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.lang.Math.random
+import java.net.URL
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.properties.Delegates
@@ -22,6 +30,7 @@ import kotlin.random.Random.Default.nextInt
 
 class MapHistoryFragment : Fragment() {
     private lateinit var viewOfLayout: View
+    private lateinit var client: OkHttpClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +43,11 @@ class MapHistoryFragment : Fragment() {
         val historyGraph: GraphView = viewOfLayout.findViewById(R.id.mapHistoryGraph)
         historyGraph.viewTreeObserver.addOnGlobalLayoutListener(GetMapLocation())
 
+        // Create OkHttp Client.
+        client = OkHttpClient()
+        Log.d("status", "Fetching sessions...")
+        fetch("http://3.72.195.76/api/sessions")
+
         // Setup dropdown menu
         // Todo: Remove dummy data and populate spinner with actual data fetched from the backend.
         val spinner: Spinner = viewOfLayout.findViewById(R.id.moverHistorySpinner)
@@ -42,6 +56,57 @@ class MapHistoryFragment : Fragment() {
         spinner.adapter = adapter
 
         return viewOfLayout
+    }
+
+    private fun fetch(sUrl: String): Sessions? {
+        var sessions: Sessions? = null
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val result = getRequest(sUrl)
+
+            if (result != null) {
+
+                Log.d("RESULT", result)
+
+                /*
+                try {
+                    // Parse result string JSON to data class
+                    sessions = Klaxon().parse<Sessions>(result)
+
+                    withContext(Dispatchers.Main) {
+                    Log.d("RESULT", "Success!")
+                }
+
+                } catch (error: Error) {
+                    Log.d("ERROR", error.toString())
+                }
+                */
+            } else {
+                Log.d("ERROR", "Request returned no response!")
+            }
+        }
+
+        return sessions
+    }
+
+    private fun getRequest(sUrl: String): String? {
+        var result: String? = null
+
+        try {
+            // Create URL
+            val url = URL(sUrl)
+
+            // Build request
+            val request = Request.Builder().url(url).build()
+
+            // Execute request
+            val response = client.newCall(request).execute()
+            result = response.body?.string()
+        } catch (error: Error) {
+            Log.d("ERROR", error.toString())
+        }
+
+        return result
     }
 
     internal inner class GetMapLocation: ViewTreeObserver.OnGlobalLayoutListener {
@@ -65,9 +130,10 @@ class MapHistoryFragment : Fragment() {
                 myList.add(randomDataPoint)
             }
 
-
             mapView.setData(myList)
         }
     }
 
 }
+
+data class Sessions(val sessionId: Int, val robotState: String, val isCollision: Boolean)
