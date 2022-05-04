a74111ab1f11fb79f86744d3c6ea574729779f44
khaleelElias <khaleel.13@hotmail.com>
2022-05-05 00:13:28 +0200

    setOnTouchListener
    aa

--- App/app/src/main/java/com/example/myapplication/ControlFragment.kt
+++ App/app/src/main/java/com/example/myapplication/ControlFragment.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_control.*
 
+
 class ControlFragment : Fragment(R.layout.fragment_control) {
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -17,20 +22,44 @@ class ControlFragment : Fragment(R.layout.fragment_control) {
                 (activity as MainActivity?)?.writeData(9); // Manuel
         }
 
-        controlUp.setOnClickListener {
-            (activity as MainActivity?)?.writeData(1);
+
+        controlUp.setOnTouchListener { _, event ->              //Forward
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                Log.d("kalle", "true!")
+                (activity as MainActivity?)?.writeData(1);
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                Log.d("kalle", "false!")
+                (activity as MainActivity?)?.writeData(0);
+            }
+            false
         }
 
-        controlLeft.setOnClickListener {
-            (activity as MainActivity?)?.writeData(2);
+        controlLeft.setOnTouchListener { _, event ->              //Left
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                (activity as MainActivity?)?.writeData(2);
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                (activity as MainActivity?)?.writeData(0);
+            }
+            false
         }
 
-        controlRight.setOnClickListener {
-            (activity as MainActivity?)?.writeData(3);
+        controlRight.setOnTouchListener { _, event ->              //Right
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                (activity as MainActivity?)?.writeData(3);
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                (activity as MainActivity?)?.writeData(0);
+            }
+            false
         }
 
-        controlDown.setOnClickListener {
-            (activity as MainActivity?)?.writeData(4);
+        controlDown.setOnTouchListener { _, event ->              //Down
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                (activity as MainActivity?)?.writeData(4);
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                (activity as MainActivity?)?.writeData(0);
+            }
+            false
+
         }
     }
 }
